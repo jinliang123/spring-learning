ee8cbc077bc315a94dad54e5493a07f7d48b4629
金亮 <liang.jin@hand-china.com>
2019-01-01 15:54:48 +0800

    [INIT]初始化项目

---
+++ spring-proxy/src/main/java/com/jinliang/proxy/jdk/Person.java
@@ -0,0 +1,13 @@
+package com.jinliang.proxy.jdk;
+
+/**
+ * description
+ *
+ * @author jinliang 2019/01/01 15:49
+ */
+public interface Person {
+    /**
+     * 寻找真爱
+     */
+    void findLove();
+}
---
+++ spring-proxy/src/main/java/com/jinliang/proxy/jdk/Zhangsan.java
@@ -0,0 +1,12 @@
+package com.jinliang.proxy.jdk;
+
+/**
+ * 张三 单身狗
+ *
+ * @author jinliang 2019/01/01 15:50
+ */
+public class Zhangsan implements Person {
+    public void findLove() {
+
+    }
+}
